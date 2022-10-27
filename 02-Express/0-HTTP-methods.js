f38786659c092cdffcca1aa2bbc9b92a1f98367d
Codatrix <ankur.codatrix@gmail.com>
2022-10-27 19:51:53 +0530

    express basics

---
+++ 02-Express/0-HTTP-methods.js
@@ -0,0 +1,10 @@
+// GET     -> Read Data
+// POST    -> Insert Data
+// PUT     -> Update Data
+// DELETE  -> Delete Data
+
+// GET        www.store.com/api/orders      get all orders
+// POST       www.store.com/api/orders      place an order (send data)
+// GET        www.store.com/api/:id         get single order (path params)
+// PUT        www.store.com/api/:id         update specific order (path params + send data)
+// DELETE     www.store.com/api/:id         delete specific order (path params)
